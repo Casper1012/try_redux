9f4082d598483d6b97be907b8d5c1a5cbae9a2f1
Casper <casperdanielsen1012@gmail.com>
2024-12-25 00:33:50 +0900

    second merge checkout

--- src/App.js
+++ src/App.js
@@ -5,12 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>I added sth new</p>
-        {console.log("hello world!")}
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <p>This is second merge</p>
+        {console.log("This is merge chek")}
         
         <a
           className="App-link"
@@ -18,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Git HUB
         </a>
       </header>
     </div>
