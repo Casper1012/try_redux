ea68729f1848538d78e0cf7905e9f229d2b3d838
Casper <casperdanielsen1012@gmail.com>
2024-12-24 23:50:44 +0900

    now working on another branch

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        
+        <p>I added sth new</p>
         {console.log("hello world!")}
         
         <a
