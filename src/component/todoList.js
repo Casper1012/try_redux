bcae7a328ca5c22975e91216f481156315b38a32
Casper <casperdanielsen1012@gmail.com>
2024-12-25 00:22:31 +0900

    test merge

---
+++ src/component/todoList.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const todoList = () => {
+    <img src={logo}/>
+    console.log("Hello true color...");
+}
+
+export default todoList;
