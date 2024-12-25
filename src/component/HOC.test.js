75b7af5f28d95cc015cd40ae28921b9f8c74e5d7
Casper <casperdanielsen1012@gmail.com>
2024-12-26 01:36:12 +0900

    flag one

---
+++ src/component/HOC.test.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+
+const withData = (WrappedComponent) => {
+  const WithData = (props) => {
+    const [data, setData] = useState([]);
+    console.log("hello");
+    s
+    useEffect(() => {
+      fetch('https://api.example.com/data')
+        .then((response) => response.json())
+        .then((data) => setData(data));
+    }, []);
+
+    return <WrappedComponent data={data} {...props} />;
+  };
+
+  return WithData;
+};
