3fd09374c84383d0a8b7a9e9d680f15bd0438ca6
Casper <casperdanielsen1012@gmail.com>
2024-12-26 02:32:50 +0900

    flag eight

--- src/component/todoList.js
+++ src/component/todoList.js
@@ -3,9 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const todoList = () => {
-    console.log("another try changed");
-    console.log("another try changed");
-    console.log("another try changed");
+    console.log("another try changed")
     <img src={logo}/>
 }
 
