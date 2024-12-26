a5e0a684e60bf6cd14151c672c5a0a1eb9d2c4bc
Casper <casperdanielsen1012@gmail.com>
2024-12-26 11:30:35 +0900

    tiny we

--- src/component/todoList.js
+++ src/component/todoList.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const todoList = () => {
-    console.log("tiny little ");
+    console.log("tiny little sdsdfsdfd");
     <img src={logo}/>
 }
 
